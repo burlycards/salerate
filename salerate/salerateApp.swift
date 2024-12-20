19b1c6af1411f7204e90530bf253b1731ecbd215
burlycards <eric@burlycards.com>
2024-12-20 11:29:48 -0600

    Initial Commit

---
+++ salerate/salerateApp.swift
@@ -0,0 +1,17 @@
+//
+//  salerateApp.swift
+//  salerate
+//
+//  Created by Eric Huisken on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct salerateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
